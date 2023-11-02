b982e541ef6bc0d2d3bd95a09084cd4484ed87a6
Raman kumar <ramankumarlal@outlook.com>
2023-11-02 15:07:32 +0530

    learing react with project

---
+++ note.md
@@ -0,0 +1,11 @@
+why to learn React?
+it is easy to manage and bulid complex front-end.
+
+![Alt text](image.png)
+
+![Alt text](image-1.png)
+![Alt text](image-2.png)
+![Alt text](image-3.png)
+Backend as  a service (BAAP)
+
+![Alt text](image-4.png)
---
+++ script.js
@@ -0,0 +1,9 @@
+function addNote() {
+    var newP = document.createElement("p");
+    var textNode = document.createTextNode("rishabh is a good boy");
+    newP.appendChild(textNode);
+    var firstElement = document.getElementsByClassName("first")[0]; // Select the first element with the class "first"
+    firstElement.appendChild(newP);
+}
+
+addNote();
